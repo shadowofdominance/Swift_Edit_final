a0635275a9ab9f396f2563d5471c86902cd4f764
Gourav <gourav@gmail.com>
2025-05-10 11:20:17 +0530

    Fixed the bugs related to find and replace currently working on Scrolling line numbers!

--- obj/Debug/net9.0-windows/Swift_Edit.AssemblyInfo.cs
+++ obj/Debug/net9.0-windows/Swift_Edit.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Swift_Edit")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a763a787e54391b2774b8ea2b03669c34d75ca7a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d58f669fdb562afde6870bf0c97c1947f2c59e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Swift_Edit")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Swift_Edit")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
